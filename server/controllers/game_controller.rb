75398e5234f7a426ba71b2c5b68d3431e5d6f082
Guilherme Moretti <guilhermebmoretti@gmail.com>
2012-07-12 03:16:49 -0300

    envia lista de territorios ao cliente conectado

--- server/controllers/game_controller.rb
+++ server/controllers/game_controller.rb
@@ -10,11 +10,16 @@ class GameController < AppController
   end
   
   def new_conn(conn)
-    get_colors(conn)    
+    get_colors(conn)
+    get_territories(conn)  
   end
   
   def get_colors(conn)
     conn.send_msg(Message.new('game','set_colors',@game.colors))
+  end
+  
+  def get_territories(conn)
+    conn.send_msg(Message.new('game','set_territories',@game.territories))
   end
   
 end

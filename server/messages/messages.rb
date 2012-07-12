6c67bf599efe3921b1184852f084701f86208680
Guilherme Moretti <guilhermebmoretti@gmail.com>
2012-07-12 01:55:50 -0300

    Classe DTO (Message) unica. Os controllers passam a ser responsaveis por montar a mensagem

--- server/messages/messages.rb
+++ server/messages/messages.rb
@@ -1,10 +1,13 @@
-require 'cgi'
-
-#Classes que encapsulam os dados a serem enviados ao cliente
 #Todas as classes herdadas de JSONable respondem aos métodos #to_json e #from_json!
 
 #classe pai de todas as mensagens enviadas ao cliente
 class Message < JSONable
+  
+  def initialize(controller,action,parameters={})
+    @controller = controller
+    @action = action
+    @params = parameters
+  end
     
   def each
     self.instance_variables.each do |v|
@@ -14,134 +17,6 @@ class Message < JSONable
   end
   
   def html_escaped(str)
-    CGI.escapeHTML(str)
+    CGI::escapeHTML(str)
   end    
 end
-
-class WarnMessage < Message
-  def initialize(msg)
-    @controller = 'chat'
-    @action = 'warn'
-    @params = { 'msg' => html_escaped(msg) }    
-  end
-end
-
-class TxtMessage < Message
-  def initialize(author,msg)
-    @controller = 'chat'
-    @action = 'txt'    
-    @params = { 'author' => author.to_s, 
-                'msg'    => html_escaped(msg) }
-  end
-end
-
-class PlayerListMessage < Message
-  def initialize(list)
-    @controller = 'playerList'
-    @action = 'update'
-    @params = { 'list' => list }    
-  end
-end
-
-class SetNewNick < Message
-  def initialize
-    @controller = 'setNick'
-    @action = 'open'
-    @params = ''
-  end
-end
-
-class ListRooms < Message
-  def initialize(list)
-    @controller = 'rooms'
-    @action = 'list'
-    
-    rooms = []
-    list.each do |r|
-      players = []
-      
-      r.players.each do |p|
-        players.push({
-            'nick' => p.nick,
-            'color' => p.color,
-            'ready' => p.ready
-          }
-        )
-      end
-      
-      rooms.push({
-        'id' => r.id,       
-        'name' => r.name,
-        'owner' => r.owner,
-        'size' => '8',
-        'players' => players
-        }
-      )
-    end
-    
-    @params = { 'list' => rooms }
-  end
-end
-
-class OpenListRooms < Message
-  def initialize
-    @controller = 'rooms'
-    @action = 'open'
-  end
-end
-
-class RoomUpdate < Message
-  def initialize(room)
-    @controller = 'pregame'
-    @action = 'update'
-    
-    players = []
-      
-    room.players.each do |p|
-      players.push({
-          'nick' => p.nick,
-          'color' => p.color,
-          'ready' => p.ready
-        }
-      )
-    end
-    
-    @params = {
-      'id' => room.id,       
-      'name' => room.name,
-      'owner' => room.owner,
-      'size' => '8',
-      'players' => players
-      }
-  end
-end
-
-class PlayerUpdate < Message
-  def initialize(p,i)
-    @controller = 'pregame'
-    @action = 'update_player'
-    @params = {
-      'index' => i,
-      'nick' => p.nick,
-      'color' => p.color,
-      'ready' => p.ready
-    }
-  end
-end
-
-class PregameShow < Message
-  def initialize
-    @controller = 'pregame'
-    @action = 'open'
-    @params = ''
-  end
-end
-
-class ColorsList < Message
-  def initialize(c)
-    @controller = 'game'
-    @action = 'set_colors'
-    @params = c
-  end
-end
-

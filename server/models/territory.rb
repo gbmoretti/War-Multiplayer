82d2ce3f9e8dfac66506eb79f843014dd778b9ea
Guilherme Moretti <guilhermebmoretti@gmail.com>
2012-07-26 01:42:18 -0300

    Novo model Territory

---
+++ server/models/territory.rb
@@ -0,0 +1,21 @@
+class Territory
+
+  attr_reader :name, :id
+  attr_accessor :owner, :troops
+
+  def initialize(id,nome,owner)
+    @id = id
+    @name = nome
+    @owner = owner if owner.is_a?(Player)
+    @troops = 0
+  end
+
+  def to_hash
+    {
+      'id' => id,
+      'owner' => owner.id,
+      'troops' => troops
+    }
+  end
+
+end

c77e7a5f1847371b71c8547939e352a34f087705
Guilherme Moretti <guilhermebmoretti@gmail.com>
2014-03-06 23:51:25 -0300

    retira arquivos desnecessarios

--- coffee_watch.sh
+++
@@ -1,1 +0,0 @@
-coffee --watch -o client --join script client/wsconnection.coffee client/controllers/*.coffee client/controllers/*/*.coffee client/appcontroller.coffee client/main.coffee
--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-#sh start_redis.sh
-#sh coffee_watch.sh &
 bundle exec thin -R config.ru start
